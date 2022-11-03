2f44a9e5f6c9d84e2b1d1207423aebc5a83103cd
Rubens Oliveira <39066466+sneburdf2022@users.noreply.github.com>
2022-11-03 19:45:39 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Gostaria de ter marca de carro?");
+        String marcaCarro = ler.nextLine();
+        System.out.println("Gostaria de ter modelo de carro?");
+        String modeloCarro = ler.nextLine();
+        String comando = "";
+        Carro carro = new Carro(marcaCarro, modeloCarro);
+        int valorA = 0;
+        int valorF = 0;
+        for(int i = 0; i < 10; i++){
+            System.out.println("Gostaria de ter acelerar o carro ou frear?(digite: acelerar, frear ou sair");
+            comando = ler.nextLine();
+            if(i==5){
+                i--;
+            }
+            if(comando.equals("acelerar")){
+                System.out.println("Digite um valor inteiro para aumentar a acelerração");
+                valorA = ler.nextInt();
+                carro.Acelerar(valorA);
+            }else if(comando.equals("frear")){
+                System.out.println("Digite um valor inteiro para diminuir a acelerração");
+                valorF = ler.nextInt();
+                carro.Frear(valorF);
+            }else if(comando.equals("sair")){
+                System.out.println("Obrigado por usar o program.");
+                i=10;
+                ler.close();
+            }
+
+        }
+        
+    }
+}
---
+++ src/Carro.java
@@ -0,0 +1,38 @@
+public class Carro {
+    private String _marca ;
+    private String _modelo;
+    private int _velocidade = 0;
+    public Carro(String Marca, String Modelo){
+        _marca = Marca;
+        _modelo = Modelo;
+    }
+    public void Acelerar(int valor){
+        int velocidadeAtual = _velocidade;
+        int somaVelocidade = _velocidade + valor;
+        if(somaVelocidade >= 200){
+            _velocidade = 200;
+        }else{
+            _velocidade =somaVelocidade;
+        }
+        System.out.println("A velocidade atual é " + velocidadeAtual + " Km/H");
+        System.out.println("Valor aumentado é " + valor );
+        System.out.println("Valor atualizado é " + _velocidade );
+    
+
+    }
+    public void Frear(int valor){
+        int velocidadeAtual = _velocidade;
+        int reduzidoVelocidade = _velocidade - valor;
+        if(reduzidoVelocidade <= 0){
+            _velocidade = 0;
+        }else{
+            _velocidade = reduzidoVelocidade;
+        }
+        System.out.println("A velocidade atual é " + velocidadeAtual + " Km/H");
+        System.out.println("Valor informado é " + valor );
+        System.out.println("Valor atualizado é " + _velocidade );
+    
+
+    }
+
+}
