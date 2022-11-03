50dd4af2f2bc497420546d99df49e2e6cc9dbaf2
Rubens Oliveira <39066466+sneburdf2022@users.noreply.github.com>
2022-11-03 20:02:54 -0300

    Refatoração

--- src/Carro.java
+++ src/Carro.java
@@ -1,38 +1,43 @@
 public class Carro {
-    private String _marca ;
+    private String _marca;
     private String _modelo;
     private int _velocidade = 0;
-    public Carro(String Marca, String Modelo){
+
+    public Carro(String Marca, String Modelo) {
         _marca = Marca;
         _modelo = Modelo;
     }
-    public void Acelerar(int valor){
+
+    public void Acelerar(int valor) {
         int velocidadeAtual = _velocidade;
         int somaVelocidade = _velocidade + valor;
-        if(somaVelocidade >= 200){
+        if (somaVelocidade >= 200) {
             _velocidade = 200;
-        }else{
-            _velocidade =somaVelocidade;
+        } else {
+            _velocidade = somaVelocidade;
         }
         System.out.println("A velocidade atual é " + velocidadeAtual + " Km/H");
-        System.out.println("Valor aumentado é " + valor );
-        System.out.println("Valor atualizado é " + _velocidade );
-    
+        System.out.println("Valor aumentado é " + valor);
+        System.out.println("Valor atualizado é " + _velocidade);
 
     }
-    public void Frear(int valor){
+
+    public void Frear(int valor) {
         int velocidadeAtual = _velocidade;
         int reduzidoVelocidade = _velocidade - valor;
-        if(reduzidoVelocidade <= 0){
+        if (reduzidoVelocidade <= 0) {
             _velocidade = 0;
-        }else{
+        } else {
             _velocidade = reduzidoVelocidade;
         }
         System.out.println("A velocidade atual é " + velocidadeAtual + " Km/H");
-        System.out.println("Valor informado é " + valor );
-        System.out.println("Valor atualizado é " + _velocidade );
-    
+        System.out.println("Valor informado é " + valor);
+        System.out.println("Valor atualizado é " + _velocidade);
 
+    }
+    
+    public void printStatus() {
+        System.out.println("Carro [_marca=" + _marca + ", _modelo=" + _modelo + ", _velocidade=" + _velocidade + "]");
     }
 
 }
--- src/ExercicioLPOO1.java
+++ src/ExercicioLPOO1.java
@@ -2,36 +2,15 @@ import java.util.Scanner;
 
 public class ExercicioLPOO1 {
     public static void main(String[] args) throws Exception {
-        Scanner ler = new Scanner(System.in);
-        System.out.println("Gostaria de ter marca de carro?");
-        String marcaCarro = ler.nextLine();
-        System.out.println("Gostaria de ter modelo de carro?");
-        String modeloCarro = ler.nextLine();
-        String comando = "";
-        Carro carro = new Carro(marcaCarro, modeloCarro);
-        int valorA = 0;
-        int valorF = 0;
-        for(int i = 0; i < 10; i++){
-            System.out.println("Gostaria de ter acelerar o carro ou frear?(digite: acelerar, frear ou sair");
-            comando = ler.nextLine();
-            if(i==5){
-                i--;
-            }
-            if(comando.equals("acelerar")){
-                System.out.println("Digite um valor inteiro para aumentar a acelerração");
-                valorA = ler.nextInt();
-                carro.Acelerar(valorA);
-            }else if(comando.equals("frear")){
-                System.out.println("Digite um valor inteiro para diminuir a acelerração");
-                valorF = ler.nextInt();
-                carro.Frear(valorF);
-            }else if(comando.equals("sair")){
-                System.out.println("Obrigado por usar o program.");
-                i=10;
-                ler.close();
-            }
+        Carro c1 = new Carro("FIAT", "Palio");
+        c1.printStatus();
+        c1.Acelerar(20);
+        c1.Acelerar(50);
+        c1.Acelerar(200);
+        c1.Frear(100);
+        c1.Frear(150);
+        c1.printStatus();
 
-        }
-        
     }
+
 }
